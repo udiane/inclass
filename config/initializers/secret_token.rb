7601622ab0f7d547c606f2e5b2848ca321e5cf5d
diane.uwacu <diane.uwacu@eagles.oc.edu>
2013-01-28 19:52:33 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inclass::Application.config.secret_token = 'ca9b2fdbfd3e48c0ae72bac92959fa20fd53902710c9ad24e491ad1dac767499e96255a12ed254403f52379fad16e263f516e839641929d3c1c70a883ea36ecf'
